f7bb7faca09e2206dfed77a4db6eb32d37ba1b9f
harshasukhdeve <harshasukhdeve0903@gmail.com>
2020-05-14 11:29:24 +0530

    FSJ0420A115

--- src/controller/PokemonController.java
+++ src/controller/PokemonController.java
@@ -10,6 +10,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.Bulbasaur;
+import model.Charizard;
+import model.Dragonite;
+import model.Mew;
+import model.Pikachu;
+
 @WebServlet(urlPatterns = { "/pokemon" })
 public class PokemonController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -45,6 +51,17 @@ public class PokemonController extends HttpServlet {
 			request.setAttribute("type2",//getter;
 			request.setAttribute("evolution2", //getter);
 			request.setAttribute("baseExp2", //getter);*/
+			
+			
+			Bulbasaur b=new Bulbasaur("Bulbasaur",001,"Bulbasaur is a Grass/Poison-type Pokémon introduced in Generation I.It evolves into Ivysaur starting at level 16. It is one of the three Starter Pokémon in the Kanto region.",
+					"POISON","IVYSAUR","64");
+			  
+			msg = "<center>" + b.getPokemonName() + "  #" + b.getPokemonNumber();
+			request.setAttribute("message1",b.getPokemonNumber());
+			request.setAttribute("character1", b.getCharacteristics());
+			request.setAttribute("type1",b.getType());
+			request.setAttribute("evolution1",b.getEvolution());
+			request.setAttribute("baseExp1", b.getBaseExp());
 		}
 
 		if (value2 != null) {
@@ -57,6 +74,18 @@ public class PokemonController extends HttpServlet {
 			request.setAttribute("type2",//getter;
 			request.setAttribute("evolution2", //getter);
 			request.setAttribute("baseExp2", //getter);*/
+			
+			Charizard c= new Charizard("Charizard",006,"\"Charizard is fit and strong and has the ability to soar up to ridiculous heights. With a single breath of fire, it can burn down forests and melt gigantic glaciers.",
+					"FIRE","CHARMELEON","240"); 
+			
+			
+				  
+			msg = "<center>" + c.getPokemonName() + "  #" +  c.getPokemonNumber();
+			request.setAttribute("message2", c.getPokemonNumber());
+			request.setAttribute("character2",c.getCharacteristics());
+			request.setAttribute("type2",c.getType());
+			request.setAttribute("evolution2", c.getEvolution());
+			request.setAttribute("baseExp2",c.getBaseExp());
 		}
 
 		if (value3 != null) {
@@ -70,6 +99,15 @@ public class PokemonController extends HttpServlet {
 			request.setAttribute("type2",//getter;
 			request.setAttribute("evolution2", //getter);
 			request.setAttribute("baseExp2", //getter);*/
+			
+			 Dragonite d=new  Dragonite(" Dragonite",149,"Dragonite is capable of flying faster than the speed of sound.It is a kindhearted Pokémon with human-like intelligence. It shows signs of altruism.", "DRAGON","DRATINI","270"); 
+			  
+				msg = "<center>" + d.getPokemonName() + "  #" +  d.getPokemonNumber();
+				request.setAttribute("message3",d.getPokemonNumber());
+				request.setAttribute("character3", d.getCharacteristics());
+				request.setAttribute("type3",d.getType());
+				request.setAttribute("evolution3",d.getEvolution());
+				request.setAttribute("baseExp3", d.getBaseExp());
 		}
 		if (value4 != null) {
 
@@ -81,6 +119,16 @@ public class PokemonController extends HttpServlet {
 			request.setAttribute("type2",//getter;
 			request.setAttribute("evolution2", //getter);
 			request.setAttribute("baseExp2", //getter);*/
+			
+			Mew m=new Mew("Mew",151,"Mew is a Psychic-type Mythical Pokémon introduced in Generation I. Mew is notable for its unique ability to learn every Technical Machine, Technical Record & Hidden Machine.",
+					"PSYCHIC","NONE","64");
+			  
+			msg = "<center>" + m.getPokemonName() + "  #" +  m.getPokemonNumber();
+			request.setAttribute("message4",m.getPokemonNumber());
+			request.setAttribute("character4",m.getCharacteristics());
+			request.setAttribute("type4",m.getType());
+			request.setAttribute("evolution4", m.getEvolution());
+			request.setAttribute("baseExp4", m.getBaseExp());
 		}
 
 		if (value5 != null) {
@@ -94,6 +142,18 @@ public class PokemonController extends HttpServlet {
 			request.setAttribute("type2",//getter;
 			request.setAttribute("evolution2", //getter);
 			request.setAttribute("baseExp2", //getter);*/
+			
+			
+			Pikachu p=new Pikachu("Pikachu",25,"Pikachu is an Electric-type Pokémon, which was introduced in Generation I. Over the years, Pikachu has become so popular that it serves as the Pokémon franchise mascot.",
+					"ELECTRIC","RAICHU","112");
+
+			msg = "<center>" + p.getPokemonName() + "  #" +  p.getPokemonNumber();
+			request.setAttribute("message5",p.getPokemonNumber());
+			request.setAttribute("character5",p.getCharacteristics());
+			request.setAttribute("type5",p.getType());
+			request.setAttribute("evolution5",p.getEvolution());
+			request.setAttribute("baseExp5", p.getBaseExp());
+
 
 		}
 
